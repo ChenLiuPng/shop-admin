37b5bae92e226d5901a8d16bc2c0f8487c94528a
chenliupeng <vclp@163.com>
2022-09-17 22:45:18 +0800

    vue3

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="home">
-      <h2>home</h2>
+      后台首页 <el-button class="" @click="addCount">{{count}}</el-button><el-button class="" @click="addCount2">{{form.count}}</el-button>
     </div>
 </template>
 
 <script setup>
+import { reactive, ref } from 'vue';
 
+  let count = ref(1);
+  const form = reactive({
+    count:0
+  })
+  const addCount = () => {
+    count.value += 1;
+  }
+  const addCount2 = () => {
+    form.count++;
+  }
 </script>
 
 
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -4,26 +4,37 @@
       <div>欢迎光临</div>
       <div>此站点是《vue3 + vite + windcss + elementplus》实战演示</div>
     </el-col>
-    <el-col
-      :lg="8"
-      :md="12"
-      class="right"
-    >
+    <el-col :lg="8" :md="12" class="right">
       <h2 class="title">欢迎回来</h2>
       <div>
         <span class="line"></span>
         <span>帐号密码登录</span>
         <span class="line"></span>
       </div>
-      <el-form :model="form" class="w-[250px]">
-        <el-form-item>
-          <el-input v-model="form.username" placeholder="请输入用户名" />
+      <el-form  :rules="rules" :model="form" class="w-[250px]">
+        <el-form-item prop="username">
+          <el-input v-model="form.username" placeholder="请输入用户名">
+            <template #prefix>
+              <el-icon><user /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="form.password" placeholder="请输入密码">
+            <template #prefix>
+              <el-icon><lock /></el-icon>
+            </template>
+          </el-input>
         </el-form-item>
         <el-form-item>
-          <el-input v-model="form.password" placeholder="请输入密码" />
-        </el-form-item>
-        <el-form-item>
-          <el-button class="w-[250px]" round color="#626aef" type="primary" @click="onSubmit">登录</el-button>
+          <el-button
+            class="w-[250px]"
+            round
+            color="#626aef"
+            type="primary"
+            @click="onSubmit"
+            >登录</el-button
+          >
         </el-form-item>
       </el-form>
     </el-col>
@@ -32,12 +43,29 @@
 
 <script setup>
 import { reactive } from "vue";
-
+import {User,Lock} from '@element-plus/icons-vue'
 // do not use same name with ref
 const form = reactive({
   username: "",
   password: "",
 });
+
+const rules = {
+  username: [
+    {
+      required: true,
+      message: "用户名不能为空",
+      trigger: "blur",
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: "密码不能为空",
+      trigger: "blur",
+    },
+  ],
+};
 
 const onSubmit = () => {
   console.log("submit!");
@@ -48,22 +76,23 @@ const onSubmit = () => {
 .login-container {
   @apply bg-indigo-500 min-h-screen;
 }
-.login-container .left,.login-container .right {
+.login-container .left,
+.login-container .right {
   @apply flex items-center justify-center flex-col;
 }
 .login-container .right {
   @apply bg-light-50 flex  flex-col;
 }
-.left>div:first-child {
+.left > div:first-child {
   @apply font-bold text-5xl text-light-50 mb-4;
 }
-.left>div:last-child {
+.left > div:last-child {
   @apply text-gray-200 text-sm;
 }
 .right .title {
   @apply font-bold text-3xl text-gray-800;
 }
-.right>div {
+.right > div {
   @apply flex items-center justify-center my-5 text-gray-300 space-x-2;
 }
 .right .line {
